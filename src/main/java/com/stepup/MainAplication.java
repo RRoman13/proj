3f7a8fbc0c6b1ec0d9da72dc9d5a6d0ea7dbf347
Roman Raspopov <rasromzes13@gmail.com>
2025-12-28 16:56:43 +0300

    Изменение методов doSomething и main для Task-002

--- src/main/java/com/stepup/MainAplication.java
+++ src/main/java/com/stepup/MainAplication.java
@@ -3,11 +3,12 @@ package com.stepup;
 public class MainAplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Новая строка");
+        System.out.println("Изменили строку для Task-001");
         System.out.println("Новая строка 3");
     }
 
     public static void doSomething () {
-        System.out.println("Новый метод");
+        System.out.println("Изменили и эту строку для Task-002");
+        System.out.println("Добавили еще строку для Task-002");
     }
 }
