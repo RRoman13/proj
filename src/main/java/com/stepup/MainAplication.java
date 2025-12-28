5492bb9c7063d28fb0acad51d92e83a1c8d5a981
Roman Raspopov <rasromzes13@gmail.com>
2025-12-28 16:51:07 +0300

    Изменение методов doSomething и main

--- src/main/java/com/stepup/MainAplication.java
+++ src/main/java/com/stepup/MainAplication.java
@@ -5,9 +5,11 @@ public class MainAplication {
         System.out.println("Hello world!");
         System.out.println("Новая строка");
         System.out.println("Новая строка 3");
+        System.out.println("Новая строка в новой ветке");
     }
 
     public static void doSomething () {
         System.out.println("Новый метод");
+        System.out.println("Новая строка в методе");
     }
 }
