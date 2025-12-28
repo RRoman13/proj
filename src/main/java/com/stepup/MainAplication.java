a34d891e0f4109a57294c5ff56428cb651ca4201
Roman Raspopov <rasromzes13@gmail.com>
2025-12-28 16:05:52 +0300

    Доработка проекта

--- src/main/java/com/stepup/MainAplication.java
+++ src/main/java/com/stepup/MainAplication.java
@@ -3,5 +3,6 @@ package com.stepup;
 public class MainAplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Новая строка");
     }
 }
